05d24abb24f9a8227a6f8dd04600f49d8e903a66
Annie <annieli@Annies-MacBook-Pro.local>
2019-02-06 09:57:20 -0800

    inut

--- studio2/script.js
+++ studio2/script.js
@@ -11,11 +11,11 @@ var deer = document.querySelector('#deer');
 var paragraph = document.querySelector('#paragraph');
 
 banner.addEventListener('click',function(){
-  paragraph.innerHTML = 'I like to travel!';
+  paragraph.innerHTML = 'I like to travel! This image is from a hiking trail in Arcata, CA.';
 });
 
 laptop.addEventListener('click',function(){
-  paragraph.innerHTML = 'I always use my laptop to do research on places to go to!';
+  paragraph.innerHTML = 'My laptop is a MacBook Pro 2016 edition. I always use it to research places to go to!';
 });
 
 bench.addEventListener('click',function(){
@@ -23,13 +23,13 @@ bench.addEventListener('click',function(){
 });
 
 phone.addEventListener('click',function(){
-  paragraph.innerHTML = 'I used my phone to navigate where I go cause I am bad with directions.';
+  paragraph.innerHTML = 'My phone is a Samsung Note 5. I used it to navigate where I go because I am bad with directions.';
 });
 
 charger.addEventListener('click',function(){
-  paragraph.innerHTML = 'Just in case my phone is out of battery. Anker chargers are reliable!';
+  paragraph.innerHTML = 'Recently my battery has been draining faster than usual. To prevent any unconveniences, Anker chargers are fast charging and reliable!';
 });
 
 deer.addEventListener('click',function(){
-  paragraph.innerHTML = 'First time I encountered a deer!';
+  paragraph.innerHTML = 'I love animals. This is my first time I encountered a deer! So rare to be so close to wildlife.';
 });
