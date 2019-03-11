62fd73a802f9d6f3e60a4eaf4f0bbcd152530424
Annie <annieli@campus-041-031.ucdavis.edu>
2019-03-11 11:17:58 -0700

    inut

--- proto2/script.js
+++ proto2/script.js
@@ -37,5 +37,5 @@ var video = document.getElementById('video');
 
 // take the photo
 document.getElementById("log").addEventListener("click", function() {
-	context.drawImage(video, 0, 0, 640, 480);
+	context.drawImage(video, 0, 0, 515, 480);
 });
