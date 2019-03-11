bb5dda1bd9b79568bc97e49aa16ce98ece098d11
Annie <annieli@campus-041-031.ucdavis.edu>
2019-03-11 10:41:06 -0700

    inut

--- proto2/script.js
+++ proto2/script.js
@@ -1,11 +1,9 @@
 "use strict";
 
-// Grab elements, create settings, etc.
 var video = document.getElementById('video');
 
-// Get access to the camera!
+// camera access
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
@@ -32,12 +30,12 @@ else if(navigator.getUserMedia) { // Standard
 }
 */
 
-// Elements for taking the snapshot
+// for photo
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var video = document.getElementById('video');
 
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
+// take the photo
+document.getElementById("log").addEventListener("click", function() {
 	context.drawImage(video, 0, 0, 640, 480);
 });
